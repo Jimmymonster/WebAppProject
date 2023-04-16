c51f2bac786fc4a61f46464889d0e64f09adff38
Jimmymonster <twosoul1212312121@gmail.com>
2023-04-16 21:24:46 +0700

    clean up code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,13 +6,13 @@ namespace TestProject.Controllers
 {
     public class UserController : Controller
     {
-        [HttpGet]
-        public ActionResult Login()
+        //[HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Login(User user)
+        public IActionResult Login(User user)
         {
             string mysqlCon = "Server=85.10.205.173;" +
             "Port=3306;" +
@@ -27,12 +27,13 @@ namespace TestProject.Controllers
                 MySqlDataReader reader = mySqlCommand.ExecuteReader();
                 if (reader.Read())
                 {
-                    String ID = reader.GetString(0);
-                    String Name = reader.GetString(1);
-                    String Pass = reader.GetString(2);
-                    String Status = reader.GetString(3);
+                    //String ID = reader.GetString(0);
+                    //String Name = reader.GetString(1);
+                    //String Pass = reader.GetString(2);
+                    //String Status = reader.GetString(3);
+                    //checking variables...
                     mySqlConnection.Close();
-                    return View("Menu");
+                    return View("~/Views/AfterLoginPage/Menu.cshtml");
                 }
                 else
                 {
@@ -51,50 +52,50 @@ namespace TestProject.Controllers
                 mySqlConnection.Close();
             }
         }
-        [HttpGet]
-        public ActionResult loginfailed()
+        //[HttpGet]
+        public IActionResult loginfailed()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult loginfailed(User user)
-        {
-            string mysqlCon = "Server=85.10.205.173;" +
-            "Port=3306;" +
-            "database=rubsarbrurueng;" +
-            "user=a_d_m_i_n;" +
-            "password=adminadmin;";
-            MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon);
-            try
-            {
-                mySqlConnection.Open();
-                MySqlCommand mySqlCommand = new MySqlCommand("select * from `user` where Name='" + user.Username + "'and Password='" + user.Password + "' COLLATE utf8mb4_0900_as_cs", mySqlConnection);
-                MySqlDataReader reader = mySqlCommand.ExecuteReader();
-                if (reader.Read())
-                {
-                    String ID = reader.GetString(0);
-                    String Name = reader.GetString(1);
-                    String Pass = reader.GetString(2);
-                    String Status = reader.GetString(3);
-                    mySqlConnection.Close();
-                    return View("Menu");
-                }
-                else
-                {
-                    mySqlConnection.Close();
-                    return View("Error");
-                }
-            }
-            catch (Exception ex)
-            {
-                mySqlConnection.Close();
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-                return View("Error");
-            }
-            finally
-            {
-                mySqlConnection.Close();
-            }
-        }
+        //[HttpPost]
+        //public IActionResult loginfailed(User user)
+        //{
+        //    string mysqlCon = "Server=85.10.205.173;" +
+        //    "Port=3306;" +
+        //    "database=rubsarbrurueng;" +
+        //    "user=a_d_m_i_n;" +
+        //    "password=adminadmin;";
+        //    MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon);
+        //    try
+        //    {
+        //        mySqlConnection.Open();
+        //        MySqlCommand mySqlCommand = new MySqlCommand("select * from `user` where Name='" + user.Username + "'and Password='" + user.Password + "' COLLATE utf8mb4_0900_as_cs", mySqlConnection);
+        //        MySqlDataReader reader = mySqlCommand.ExecuteReader();
+        //        if (reader.Read())
+        //        {
+        //            String ID = reader.GetString(0);
+        //            String Name = reader.GetString(1);
+        //            String Pass = reader.GetString(2);
+        //            String Status = reader.GetString(3);
+        //            mySqlConnection.Close();
+        //            return View("Menu");
+        //        }
+        //        else
+        //        {
+        //            mySqlConnection.Close();
+        //            return View("Error");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        mySqlConnection.Close();
+        //        System.Diagnostics.Debug.WriteLine(ex.Message);
+        //        return View("Error");
+        //    }
+        //    finally
+        //    {
+        //        mySqlConnection.Close();
+        //    }
+        //}
     }
 }
